eb3deda40aeb0a1022ec92f9b75e92e2c2ce2365
katecg <katecg@udel.edu>
2024-10-14 20:04:48 -0400

    Updating Detailed Quiz

--- src/DetailedQuiz.tsx
+++ src/DetailedQuiz.tsx
@@ -1,13 +1,34 @@
 //import React, { useState } from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
 
 
 const DetailedQuiz:React.FC=()=>{
     return(
-        <div>
+        <><div>
             <h1>Detailed Career Quiz</h1>
             <p>Welcome to the Detailed Career Quiz!</p>
-        </div>
+        </div><Container style={{ border: '1px solid black', margin: '10px', padding: '10px', height: '400px' }}>
+            <h2>Question One</h2>
+            How do you prefer to work on projects?
+            <Row>
+          <Col style={{ border: '1px solid black', margin: '10px', padding: '10px', height: '100px' }}>
+            <h3>Option 1</h3>
+          </Col>
+          <Col style={{ border: '1px solid black', margin: '10px', padding: '10px', height: '100px' }}>
+            <h3>Option 2</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col style={{ border: '1px solid black', margin: '10px', padding: '10px', height: '100px' }}>
+            <h3>Option 3</h3>
+          </Col>
+          <Col style={{ border: '1px solid black', margin: '10px', padding: '10px', height: '100px' }}>
+            <h3>Option 4</h3>
+          </Col>
+        </Row>
+            </Container></>
+        
     );
 };
 
