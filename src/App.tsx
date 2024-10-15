3704b4100d24c88b086e63ab2e582c440fb661b2
Morgan Nutto <xxmnxx@udel.edu>
2024-10-15 14:16:17 -0400

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
     localStorage.setItem(saveKeyData, JSON.stringify(key));
     window.location.reload(); // when making a mistake and changing the key again, reload the whole site
   }
-
+//comment
   // whenever there's a change it'll store the API key in a local state called key but it won't be set in the local storage until the user clicks the submit button
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
