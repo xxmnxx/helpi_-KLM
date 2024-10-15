cd4456909904c6dcb78b53e0a0fef0fdb6c37864
katecg <katecg@udel.edu>
2024-10-14 20:26:11 -0400

    Updating Detailed Quiz

--- src/DetailedQuiz.tsx
+++ src/DetailedQuiz.tsx
@@ -1,9 +1,11 @@
 //import React, { useState } from 'react';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Container} from 'react-bootstrap';
+// import React, { useState } from 'react';
 
 
 
 const DetailedQuiz:React.FC=()=>{
+
     return(
         <><div>
             <h1>Detailed Career Quiz</h1>
@@ -11,23 +13,9 @@ const DetailedQuiz:React.FC=()=>{
         </div><Container style={{ border: '1px solid black', margin: '10px', padding: '10px', height: '400px' }}>
             <h2>Question One</h2>
             How do you prefer to work on projects?
-            <Row>
-          <Col style={{ border: '1px solid black', margin: '10px', padding: '10px', height: '100px' }}>
-            <h3>Option 1</h3>
-          </Col>
-          <Col style={{ border: '1px solid black', margin: '10px', padding: '10px', height: '100px' }}>
-            <h3>Option 2</h3>
-          </Col>
-        </Row>
-        <Row>
-          <Col style={{ border: '1px solid black', margin: '10px', padding: '10px', height: '100px' }}>
-            <h3>Option 3</h3>
-          </Col>
-          <Col style={{ border: '1px solid black', margin: '10px', padding: '10px', height: '100px' }}>
-            <h3>Option 4</h3>
-          </Col>
-        </Row>
+
             </Container></>
+        
         
     );
 };
