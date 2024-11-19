29f1341fa785ba7bd4b5fb38cbe9047fa3d8e093
Morgan Nutto <xxmnxx@udel.edu>
2024-11-19 12:50:02 -0500

    add images and separate file

--- src/home.tsx
+++ src/home.tsx
@@ -3,6 +3,8 @@ import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 import './App.css'; // Make sure your CSS file is imported
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import basicIcon from './Images/hot-coffee.svg';
+import detailedIcon from './Images/croissant.svg';
 
 const saveKeyData = 'MYKEY';
 const prevKey = localStorage.getItem(saveKeyData);
@@ -55,6 +57,7 @@ function HomePage() {
 
         <Row>
           <Col className="custom-box">
+          <img src= {basicIcon} style={{width: '150px', height: 'auto'}}/>
             <h3>Make Your Own Coffee!</h3>
             <p>Take it easy! Make your own coffee using hand-picked ingredients, unique to your interests and skills! Ideal for those looking for a quick assessment to assist in discovering a career that best fits.</p>
             <Button
@@ -66,6 +69,7 @@ function HomePage() {
             </Button>
           </Col>
           <Col className="custom-box">
+          <img src= {detailedIcon} style={{width: '150px', height: 'auto'}}/>
             <h3>Make Your Own Pastries!</h3>
             <p>Get your hands dirty! Bake your own pastries using hand-picked ingredients, unique to your interests and skills! Ideal for those committed to investing extra time to gain deeper insights into their ideal career trajectory.</p>
             <Button
